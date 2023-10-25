5cdf361e51126947fab5f14582236695a3dea9bb
Chris Pikul <chris@workmaple.com>
2023-10-25 19:54:13 +0300

    added init command scaffold

--- cmd/root.go
+++ cmd/root.go
@@ -28,22 +28,33 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+var (
+	cfgFile string
 
-var cfgFile string
+	optDebug bool
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "goral",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Version: "0.1.0-alpha",
+	Short: "CRUD API generator for Go",
+	Long: `Automatically generates Go code designed for REST-ish API generation
+based on manifest files. With Goral entire back-end services can be generated
+using a few configuration (manifest) files written in YAML/JSON.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Some features include:
+ * Automatic struct generation for resources
+ * Endpoint binding for web servers
+ * SQL generation scripts for building the schema
+ * Exporting TypeScript declarations
+ * And much more...
+ 
+Initialize a project/repository using "goral init" to scaffold the folder
+structure and configurations. From there "goral add" will create new manifests,
+and "goral build" will re-generate the Go files.
+
+See "goral help" for more information on available commands and options.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -58,14 +69,13 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	// === Configuration settings ===
 
+	// === Persistent flags ===
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goral.yaml)")
+	rootCmd.PersistentFlags().BoolVarP(&optDebug, "debug", "D", false, "enables debug logging")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	// === Local flags ===
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
